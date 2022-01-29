f4139fb3ca45cf3e43d5dabe06d7ce184af11057
Mejbah <mejba.s@gmail.com>
2022-01-30 03:46:20 +0600

    parameter added

--- concept.js
+++ concept.js
@@ -72,3 +72,17 @@ function startFan() {
   console.log("press the switch.");
 }
 startFan();
+
+// Function Parameter
+function bringSingara(taka) {
+  console.log("singara-r jonno dese tk.", taka);
+  console.log("mama, singara den.");
+
+  var singaraPrice = 10;
+  var singaraQuantity = taka / singaraPrice;
+  return singaraQuantity;
+}
+var money = 250;
+var singara = bringSingara(money);
+
+console.log(singara);
