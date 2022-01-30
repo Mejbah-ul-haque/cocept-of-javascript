ea43dcfee8cbe6a352a7f49aab3731c43a66360e
Mejbah <mejba.s@gmail.com>
2022-01-30 18:38:02 +0600

    add switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+var color = "black";
+if (color == "blue") {
+  console.log("color is blue");
+} else if (color == "red") {
+  console.log("color is red");
+} else if (color == "green") {
+  console.log("color is green");
+} else if (color == "white") {
+  console.log("color is white");
+} else if (color == "yellow") {
+  console.log("color is yellow");
+} else {
+  console.log("color is black");
+}
