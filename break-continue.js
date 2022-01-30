9ca23da41d5a478879e0c61d70e1f4e5cce9e44c
Mejbah <mejba.s@gmail.com>
2022-01-30 19:13:42 +0600

    Break and Continue concept clear

--- break-continue.js
+++ break-continue.js
@@ -6,3 +6,27 @@ while (i < 10) {
   }
   i++;
 }
+
+for (var i = 0; i <= 20; i++) {
+  console.log(i);
+  if (i > 8) {
+    break;
+  }
+}
+
+var numbers = [21, 35, 45, 63, 65, 49, 101, 55, 66, 98];
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+  if (number > 60) {
+    break;
+  }
+}
+
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  if (number > 60) {
+    continue;
+  }
+  console.log(number);
+}
--- concept.js
+++ concept.js
@@ -209,3 +209,38 @@ switch (color) {
   default:
     console.log("color is black");
 }
+
+// Break and Continue
+
+var i = 0;
+while (i < 10) {
+  console.log(i);
+  if (i == 5) {
+    break;
+  }
+  i++;
+}
+
+for (var i = 0; i <= 20; i++) {
+  console.log(i);
+  if (i > 8) {
+    break;
+  }
+}
+
+var numbers = [21, 35, 45, 63, 65, 49, 101, 55, 66, 98];
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+  if (number > 60) {
+    break;
+  }
+}
+
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  if (number > 60) {
+    continue;
+  }
+  console.log(number);
+}
